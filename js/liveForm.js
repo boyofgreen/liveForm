cd2fe82d88950cc45f8b529123bcf59c0eda70b4
Jeff Burtoft <boyofgreen@gmail.com>
2012-03-07 05:45:37 +0000

    "more robust example"

--- js/liveForm.js
+++ js/liveForm.js
@@ -1,9 +1,36 @@
 //liveForm JS
 
 
+var pageMeter = document.querySelector('.completeMeter');
 
-document.myForm.quantity.addEventListener('input', function(e){this.checkValidity()}, false);
+var checkMyValidity = function(){
+    var valueCollection = document.querySelectorAll('#RegisterUserForm input');
+    var myTotal = 0;
+    for (var i = 0; i< valueCollection.length; i++) { 
+  
+    //store it in local storage so it's there if they come back
+    localStorage[valueCollection[i].name] = valueCollection[i].value;
+  
+    var isValid = valueCollection[i].checkValidity();
+    
+       if(isValid === true){
+             myTotal += 20;
+        
+        }
+   // console.log(myTotal);
+     pageMeter.value = myTotal;
+    }
+    
+    };
+//check to see if supports it
+if(document.createElement('progress').max != undefined){console.log('working');pageMeter.style.display = 'block';}
 
-document.myForm.quantity.addEventListener('invalid', function(e){
-alert('Your Number needs to be between 1 and five, you choose '+this.value+'.')
-}, false);
+var myInputs = document.myForm.querySelectorAll('input');
+for (var i = 0; i< myInputs.length; i++) { 
+    
+    myInputs[i].addEventListener(myInputs[i].type!=="checkbox"?'input':'change',checkMyValidity , false);
+    if(localStorage[myInputs[i].name] && localStorage[myInputs[i].name] !== 'undefined'){myInputs[i].value = localStorage[myInputs[i].name];}
+}
+
+checkMyValidity();
+
